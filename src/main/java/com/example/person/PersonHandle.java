7d62061640d6291d97dabf7d2eaf02638685be5d
jingmiao <jingmiao@chtwm.com>
2021-09-09 19:55:16 +0800

    查找有过迁移的潜在数据

--- src/main/java/com/example/person/PersonHandle.java
+++ src/main/java/com/example/person/PersonHandle.java
@@ -8,27 +8,72 @@ import com.example.ExcelTool;
 import com.example.person.entity.excelBean.ExcelPerson;
 import com.example.person.listener.PersonListener;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PersonHandle {
 
-    private static PersonListener build() {
-        ExcelReader excelReader = ExcelTool.getExcelReader("F:\\excel\\210908\\en_rd_person.xlsx");
+    private static PersonListener build(String excelFilePath) {
+        ExcelReader excelReader = ExcelTool.getExcelReader(excelFilePath);
         PersonListener listener = new PersonListener();
         ReadSheet readSheetSys = EasyExcel.readSheet().head(ExcelPerson.class).registerReadListener(listener).build();
         excelReader.read(readSheetSys);
         return listener;
     }
 
+    // 1、发明人 年份  城市
+    // 2、发明人-》所在城市超过2个的
+    // 3、发明人去重-》同一年份，不同城市，发明类型不同
+    // 4、发明人离开城市年份、进入城市年份
+    // 5、汇总进入/离开同一城市的发明人集合，区分年份
+
+    private static Map<String, List<ExcelPerson>> buildPersonList(List<ExcelPerson> list) {
+        Map<String, List<ExcelPerson>> personMap = list.parallelStream().collect(Collectors.groupingBy(ExcelPerson::getName));
+        Iterator<Map.Entry<String, List<ExcelPerson>>> it = personMap.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, List<ExcelPerson>> entry = it.next();
+            if (!checkCity(entry.getValue())) {
+                it.remove();//使用迭代器的remove()方法删除元素
+            }
+        }
+        return personMap;
+    }
+
+    /**
+     * 校验发明人个人所在城市是否超过了1个
+     * 超过2个说明该发明人是潜在的迁移数据
+     * 此处不处理重名问题
+     */
+    private static boolean checkCity(List<ExcelPerson> list) {
+        String city = null;
+        boolean isMoreCity = false;
+        for (ExcelPerson person : list) {
+            if (city == null) {
+                city = person.getCityCodeMain();
+                continue;
+            }
+            if (!city.equals(person.getCityCodeMain())) {
+                isMoreCity = true;
+                break;
+            }
+        }
+        return isMoreCity;
+    }
+
     public static void main(String[] args) {
-        PersonListener listen = build();
+        String excelFilePath = "F:\\commiao_public\\public\\小井\\jing_处理好的数据\\210908\\en_rd_person.xlsx";
+        PersonListener listen = build(excelFilePath);
         List<ExcelPerson> list = listen.getPersonList();
-        list.stream().filter(excelPerson ->
-                excelPerson.getSymbol().equals("000012")
-                        && excelPerson.getYear().equals(2013)
-                        && excelPerson.getName().equals("李大平")
-        ).forEach(
-                excelPerson -> System.out.println(JSONObject.toJSON(excelPerson))
-        );
+
+        buildPersonList(list);
+//        list.stream().filter(excelPerson ->
+//                excelPerson.getSymbol().equals("000012")
+//                        && excelPerson.getYear().equals(2013)
+//                        && excelPerson.getName().equals("李大平")
+//        ).forEach(
+//                excelPerson -> System.out.println(JSONObject.toJSON(excelPerson))
+//        );
     }
 }
--- src/main/java/com/example/person/entity/excelBean/ExcelPerson.java
+++ src/main/java/com/example/person/entity/excelBean/ExcelPerson.java
@@ -41,4 +41,9 @@ public class ExcelPerson {
      **/
     @ExcelProperty("city_code_main")
     String cityCodeMain;
+    /**
+     * 城市编码
+     **/
+    @ExcelProperty("city")
+    String city;
 }
